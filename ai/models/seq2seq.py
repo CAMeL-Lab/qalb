a8d0056b2b5d9681dff632dd6ebc0201aa6466f3
Daniel Watson <watsondaniel6@gmail.com>
2017-06-04 12:33:50 +0400

    Added residual wrappers

--- ai/models/seq2seq.py
+++ ai/models/seq2seq.py
@@ -4,6 +4,7 @@ from __future__ import division
 
 from six.moves import xrange
 import tensorflow as tf
+# pylint: disable=no-name-in-module
 from tensorflow.python.layers.core import Dense
 
 from ai.models import BaseModel
@@ -18,10 +19,10 @@ class Seq2Seq(BaseModel):
      3. A `go_id` is reserved for the model to provide to the decoder."""
   
   def __init__(self, num_types=0, max_encoder_length=99, max_decoder_length=99,
-               pad_id=0, eos_id=1, go_id=2, lr=1., lr_decay=1., batch_size=32,
-               embedding_size=128,  rnn_layers=2, bidirectional_encoder=True,
-               max_grad_norm=5, use_lstm=False, use_luong_attention=True,
-               initial_p_sample=0., **kw):
+               pad_id=0, eos_id=1, go_id=2, batch_size=32, embedding_size=128,
+               rnn_layers=2, bidirectional_encoder=True, max_grad_norm=5,
+               use_lstm=False, use_residual=False, use_luong_attention=True,
+               p_sample=0., **kw):
     """TODO: add documentation for all arguments."""
     self.num_types = num_types
     self.max_encoder_length = max_encoder_length
@@ -29,16 +30,15 @@ class Seq2Seq(BaseModel):
     self.pad_id = pad_id
     self.eos_id = eos_id
     self.go_id = go_id
-    self.lr = lr
-    self.lr_decay = lr_decay
     self.batch_size = batch_size
     self.embedding_size = embedding_size
     self.rnn_layers = rnn_layers
-    self.bidirectional_encoder = True
+    self.bidirectional_encoder = bidirectional_encoder
     self.max_grad_norm = max_grad_norm
     self.use_lstm = use_lstm
+    self.use_residual = use_residual
     self.use_luong_attention = use_luong_attention
-    self.initial_p_sample = initial_p_sample
+    self.p_sample = tf.Variable(p_sample, trainable=False, name='p_sample')
     super(Seq2Seq, self).__init__(**kw)
   
   def build_graph(self):
@@ -58,17 +58,7 @@ class Seq2Seq(BaseModel):
     decoder_seed_ids = tf.tile(
       [[self.go_id]], [self.batch_size, self.max_decoder_length]
     )
-    
-    self._lr = tf.Variable(self.lr, trainable=False, name='lr')
-    self._p_sample = tf.Variable(
-      self.initial_p_sample, trainable=False, name='p_sample'
-    )
-    
     input_lengths = self.get_sequence_length(self.inputs)
-    if self.use_lstm:
-      rnn_cell = tf.contrib.rnn.LSTMBlockCell 
-    else:
-      rnn_cell = tf.contrib.rnn.GRUBlockCell
     
     with tf.variable_scope('embeddings'):
       sqrt3 = 3 ** .5  # Uniform(-sqrt3, sqrt3) has variance 1
@@ -83,10 +73,10 @@ class Seq2Seq(BaseModel):
     with tf.variable_scope('encoder_rnn'):
       if self.bidirectional_encoder:
         encoder_cell_fw = tf.contrib.rnn.MultiRNNCell(
-          [rnn_cell(self.embedding_size) for _ in xrange(self.rnn_layers)]
+          [self.rnn_cell() for _ in xrange(self.rnn_layers)]
         )
         encoder_cell_bw = tf.contrib.rnn.MultiRNNCell(
-          [rnn_cell(self.embedding_size) for _ in xrange(self.rnn_layers)]
+          [self.rnn_cell() for _ in xrange(self.rnn_layers)]
         )
         (encoder_fw_out, encoder_bw_out), _ = tf.nn.bidirectional_dynamic_rnn(
           encoder_cell_fw, encoder_cell_bw, encoder_input, dtype=tf.float32,
@@ -95,7 +85,7 @@ class Seq2Seq(BaseModel):
         encoder_output = tf.concat([encoder_fw_out, encoder_bw_out], 2)
       else:
         encoder_cell = tf.contrib.rnn.MultiRNNCell(
-          [rnn_cell(self.embedding_size) for _ in xrange(self.rnn_layers)]
+          [self.rnn_cell() for _ in xrange(self.rnn_layers)]
         )
         encoder_output, _ = tf.nn.dynamic_rnn(
           encoder_cell, encoder_input, dtype=tf.float32,
@@ -104,8 +94,7 @@ class Seq2Seq(BaseModel):
     
     with tf.variable_scope('decoder_rnn') as scope:
       # The first RNN is wrapped with the attention mechanism
-      # TODO: add option to allow normalizing the energy term
-      decoder_cell = rnn_cell(self.embedding_size)
+      decoder_cell = self.rnn_cell()
       if self.use_luong_attention:
         attention_mechanism = tf.contrib.seq2seq.LuongAttention(
           self.embedding_size, encoder_output,
@@ -122,14 +111,14 @@ class Seq2Seq(BaseModel):
       # Stack all the cells if more than one RNN is used
       if self.rnn_layers > 1:
         decoder_cell = tf.contrib.rnn.MultiRNNCell(
-          [decoder_cell] + [rnn_cell(self.embedding_size)
+          [decoder_cell] + [self.rnn_cell()
                             for _ in xrange(self.rnn_layers - 1)]
         )
       initial_state = decoder_cell.zero_state(self.batch_size, tf.float32)
       # Training decoder
       sampling_helper = tf.contrib.seq2seq.ScheduledOutputTrainingHelper(
         decoder_input, tf.tile([self.max_decoder_length], [self.batch_size]),
-        self._p_sample
+        self.p_sample
       )
       decoder = tf.contrib.seq2seq.BasicDecoder(
         decoder_cell, sampling_helper, initial_state
@@ -161,29 +150,34 @@ class Seq2Seq(BaseModel):
     # Weighted softmax cross entropy loss
     with tf.name_scope('loss'):
       mask = tf.cast(tf.sign(self.labels), tf.float32)
-      loss = tf.contrib.seq2seq.sequence_loss(logits, self.labels, mask)
-    with tf.name_scope('perplexity'):
-      self.perplexity = tf.exp(loss)
-      tf.summary.scalar('perplexity', self.perplexity)
-    
-    # Operation for decaying learning rate
-    with tf.name_scope('decay_lr'):
-      self.decay_lr = tf.assign(self._lr, self._lr * self.lr_decay)
+      self.loss = tf.contrib.seq2seq.sequence_loss(logits, self.labels, mask)
+      tf.summary.scalar('log_perplexity', self.loss)
     
     # Optimizer clips gradients by max norm
     with tf.variable_scope('train_op'):
       tvars = tf.trainable_variables()
       grads, _ = tf.clip_by_global_norm(
-        tf.gradients(loss, tvars), self.max_grad_norm
+        tf.gradients(self.loss, tvars), self.max_grad_norm
       )
-      optimizer = tf.train.AdamOptimizer(self._lr)
-      self.train_op = optimizer.apply_gradients(
+      self.optimizer = tf.train.AdamOptimizer(self.lr)
+      self.train_op = self.optimizer.apply_gradients(
         zip(grads, tvars), global_step=self.global_step
       )
   
   def get_embeddings(self, ids):
     """Performs embedding lookup. Useful as a method for decoder helpers."""
     return tf.nn.embedding_lookup(self.embedding_kernel, ids)
+  
+  def rnn_cell(self):
+    """Get a new RNN cell with wrappers according to the initial config."""
+    cell = None
+    if self.use_lstm:
+      cell = tf.contrib.rnn.LSTMBlockCell(self.embedding_size)
+    else:
+      cell = tf.contrib.rnn.GRUBlockCell(self.embedding_size)
+    if self.use_residual:
+      cell = tf.contrib.rnn.ResidualWrapper(cell)
+    return cell
   
   def get_sequence_length(self, sequence_batch):
     """Given a 2D batch of input sequences, return a vector with the lengths
