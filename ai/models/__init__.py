a0e71192ba84d475aa61fd393711e05a1a3aa833
Daniel Watson <watsondaniel6@gmail.com>
2017-06-25 15:35:01 +0400

    Made new seq2seq default

--- ai/models/__init__.py
+++ ai/models/__init__.py
@@ -4,4 +4,3 @@ from ai.models.base_model import BaseModel
 #from ai.models.dnc import DNC
 from ai.models.next_gram import NextGram
 from ai.models.seq2seq import Seq2Seq
-from ai.models.experimental_seq2seq import ExperimentalSeq2Seq
--- ai/models/seq2seq.py
+++ ai/models/seq2seq.py
@@ -19,39 +19,87 @@ class Seq2Seq(BaseModel):
      3. A `go_id` is reserved for the model to provide to the decoder."""
   
   def __init__(self, num_types=0, max_encoder_length=99, max_decoder_length=99,
-               pad_id=0, eos_id=1, go_id=2, lr=1e-3, batch_size=32,
-               embedding_size=128, rnn_layers=2, bidirectional_encoder=True,
-               add_fw_bw=True, pyramid_encoder=False, max_grad_norm=5.,
-               epsilon=1e-8, use_lstm=False, use_residual=False,
-               use_luong_attention=True, beam_size=1, dropout=1., **kw):
-    """TODO: add documentation for all arguments."""
+               pad_id=0, eos_id=1, go_id=2,
+               adam_lr=1e-3, adam_lr_decay=1.,
+               gd_lr=.1, gd_lr_decay=1.,
+               batch_size=32, embedding_size=128, rnn_layers=2,
+               bidirectional_encoder=False, bidirectional_mode='add',
+               pyramid_encoder=False, use_lstm=False, use_residual=False,
+               attention=None, feed_inputs=False, dropout=1.,
+               max_grad_norm=5., epsilon=1e-8, beam_size=1., **kw):
+    """Keyword args:
+       `num_types`: number of unique types (e.g. vocabulary or alphabet size),
+       `max_encoder_length`: max length of the encoder,
+       `max_decoder_length`: max length of the decoder,
+       `pad_id`: the integer id that represents padding (defaults to 0),
+       `eos_id`: the integer id that represents the end of the sequence,
+       `go_id`: the integer id fed to the decoder as the first input,
+       `adam_lr`: initial learning rate for Adam optimizer,
+       `adam_lr_decay`: learning rate decay for Adam optimizer,
+       `gd_lr`: initial learning rate for gradient descent optimizer,
+       `gd_lr_decay`: learning rate decay for gradient descent optimizer,
+       `batch_size`: minibatch size,
+       `embedding_size`: integer number of hidden units,
+       `rnn_layers`: number of RNN layers for the encoder and decoder,
+       `bidirectional_encoder`: whether to use a bidirectional encoder RNN,
+       `bidirectional_mode`: string for the bidirectional RNN architecture:
+        'add' (default): add the forward and backward hidden states,
+        'project': use a projection matrix to resize the concatenation of the
+                   forward and backward hidden states to `embedding_size`,
+        'concat': concatenate the forward and backward inputs and pass that
+                  as the input to the next RNN (note: this will not allow the
+                  use of residual connections),
+       `pyramid_encoder`: whether to use a pyramid encoder that halves the
+        number of time steps per layer (Xie et al.,
+        https://arxiv.org/pdf/1603.09727),
+       `use_lstm`: set to False to use a GRU cell (Cho et al.,
+        https://arxiv.org/abs/1406.1078),
+       `use_residual`: whether to use residual connections between RNN cells
+        (Wu et al., https://arxiv.org/pdf/1609.08144.pdf),
+       `attention`: 'bahdanau', or 'luong' (none by default),
+       `feed_inputs`: set to True to feed attention-based inputs to the
+        decoder RNN (Luong et al., https://arxiv.org/abs/1508.04025),
+       `dropout`: keep probability for the non-recurrent connections between
+        RNN cells. Defaults to 1.0; i.e. no dropout,
+       `max_grad_norm`: clip gradients to maximally this norm,
+       `epsilon`: small numerical constant for AdamOptimizer (default 1e-8)."""
     self.num_types = num_types
     self.max_encoder_length = max_encoder_length
     self.max_decoder_length = max_decoder_length
     self.pad_id = pad_id
     self.eos_id = eos_id
     self.go_id = go_id
+    self.adam_lr_decay = adam_lr_decay
+    self.gd_lr_decay = gd_lr_decay
     self.batch_size = batch_size
     self.embedding_size = embedding_size
     self.rnn_layers = rnn_layers
     self.bidirectional_encoder = bidirectional_encoder
-    self.add_fw_bw = add_fw_bw
+    self.bidirectional_mode = bidirectional_mode
     self.pyramid_encoder = pyramid_encoder
-    self.max_grad_norm = max_grad_norm
-    self.epsilon = epsilon
     self.use_lstm = use_lstm
     self.use_residual = use_residual
-    self.use_luong_attention = use_luong_attention
-    self.beam_size = beam_size
+    self.attention = attention
+    self.feed_inputs = feed_inputs
     self.dropout = dropout
-    self.lr = tf.Variable(
-      0., trainable=False, dtype=tf.float32, name='learning_rate')
+    self.max_grad_norm = max_grad_norm
+    self.epsilon = epsilon
+    self.beam_size = beam_size
+    # Use graph variables for learning rates to allow them to be modified/saved
+    self.adam_lr = tf.Variable(
+      adam_lr, trainable=False, dtype=tf.float32, name='adam_learning_rate')
+    self.gd_lr = tf.Variable(
+      gd_lr, trainable=False, dtype=tf.float32, name='gd_learning_rate')
+    # Sampling probability variable that can be manually changed. See scheduled
+    # sampling paper (Bengio et al., https://arxiv.org/abs/1506.03099)
     self.p_sample = tf.Variable(
-      0., trainable=False, dtype=tf.float32, name='p_sample')
+      0., trainable=False, dtype=tf.float32, name='sampling_probability')
     super(Seq2Seq, self).__init__(**kw)
   
   
   def build_graph(self):
+    
+    # Placeholders
     self.inputs = tf.placeholder(
       tf.int32, name='inputs',
       shape=[self.batch_size, self.max_encoder_length])
@@ -61,17 +109,21 @@ class Seq2Seq(BaseModel):
     self.temperature = tf.placeholder_with_default(
       1., name='temperature', shape=[])
     
-    with tf.name_scope('decoder_inputs'):
+    # Prepare training decoder inputs
+    with tf.name_scope('train_decoder_inputs'):
       decoder_ids = tf.concat(
         [tf.tile([[self.go_id]], [self.batch_size, 1]), self.labels], 1)
     
+    # Embedding matrix
     with tf.variable_scope('embeddings'):
       sqrt3 = 3 ** .5  # Uniform(-sqrt3, sqrt3) has variance 1
       self.embedding_kernel = tf.get_variable(
         'kernel', [self.num_types, self.embedding_size],
         initializer=tf.random_uniform_initializer(minval=-sqrt3, maxval=sqrt3))
-      encoder_input = self.get_embeddings(self.inputs)
-      decoder_input = self.get_embeddings(decoder_ids)
+    
+    # Look up the embeddings for the encoder and decoder inputs
+    encoder_input = self.get_embeddings(self.inputs)
+    decoder_input = self.get_embeddings(decoder_ids)
     
     with tf.variable_scope('encoder'):
       encoder_output = self.build_encoder(encoder_input)
@@ -83,46 +135,113 @@ class Seq2Seq(BaseModel):
     # Index outputs (greedy)
     self.output = tf.argmax(logits, axis=2, name='output')
     
-    # Weighted softmax cross entropy loss
+    # Softmax cross entropy loss masked by the target sequence lengths
     with tf.name_scope('loss'):
       mask = tf.cast(tf.sign(self.labels), tf.float32)
       loss = tf.contrib.seq2seq.sequence_loss(logits, self.labels, mask)
-      self.perplexity = tf.exp(loss)
+    
+    self.perplexity = tf.exp(loss, name='perplexity')
     tf.summary.scalar('perplexity', self.perplexity)
     
-    # Optimizer clips gradients by max norm
-    with tf.variable_scope('train_op'):
+    # Adam and gradient descent optimizers with norm clipping. This prevents
+    # exploding gradients and allows a switch from Adam to SGD when the model
+    # is reaching convergence (Wu et al., https://arxiv.org/pdf/1609.08144.pdf)
+    with tf.name_scope('train_ops'):
       tvars = tf.trainable_variables()
       grads, _ = tf.clip_by_global_norm(
         tf.gradients(loss, tvars), self.max_grad_norm)
-      # Include a gradient descent op; switching can prove helpful
-      adam_optimizer = tf.train.AdamOptimizer(self.lr, epsilon=self.epsilon)
-      gradient_descent = tf.train.GradientDescentOptimizer(self.lr)
+      adam_optimizer = tf.train.AdamOptimizer(
+        self.adam_lr, epsilon=self.epsilon)
+      gradient_descent = tf.train.GradientDescentOptimizer(self.gd_lr)
       self.adam = adam_optimizer.apply_gradients(
         zip(grads, tvars), global_step=self.global_step)
       self.sgd = gradient_descent.apply_gradients(
         zip(grads, tvars), global_step=self.global_step)
+    
+    # Runnable ops for decaying the learning rates
+    with tf.name_scope('decay_lr'):
+      self.decay_adam_lr = tf.assign(
+        self.adam_lr, self.adam_lr * self.adam_lr_decay,
+        name='decay_adam_learning_rate')
+      self.decay_gd_lr = tf.assign(
+        self.gd_lr, self.gd_lr * self.gd_lr_decay,
+        name='decay_gd_learning_rate')
+  
+  
+  def get_embeddings(self, ids):
+    """Performs embedding lookup. Useful as a method for decoder helpers.
+       Note this method requires the `embedding_kernel` attribute to be
+       declared before being called."""
+    with tf.name_scope('get_embeddings'):
+      return tf.nn.embedding_lookup(self.embedding_kernel, ids)
+  
+  
+  def rnn_cell(self, num_units=None, attention_mechanism=None):
+    """Get a new RNN cell with wrappers according to the initial config."""
+    cell = None
+    
+    # Allow custom number of hidden units
+    if num_units is None:
+      num_units = self.embedding_size
+    
+    # Check to use LSTM or GRU
+    if self.use_lstm:
+      cell = tf.contrib.rnn.LSTMBlockCell(num_units)
+    else:
+      cell = tf.contrib.rnn.GRUBlockCell(num_units)
+    
+    # Check whether to add an attention mechanism
+    if attention_mechanism is not None:
+      cell = tf.contrib.seq2seq.AttentionWrapper(cell, attention_mechanism)
+    
+    # Check whether to add residual connections
+    if self.use_residual:
+      cell = tf.contrib.rnn.ResidualWrapper(cell)
+    
+    # Note: dropout should always be the last wrapper
+    if self.dropout < 1:
+      cell = tf.contrib.rnn.DropoutWrapper(
+        cell, input_keep_prob=self.dropout, output_keep_prob=self.dropout)
+    
+    return cell
+  
+  
+  def get_sequence_length(self, sequence_batch):
+    """Given a 2D batch of input sequences, return a vector with the lengths
+       of every sequence excluding the paddings."""
+    with tf.name_scope('get_sequence_length'):
+      return tf.reduce_sum(
+        tf.sign(tf.abs(sequence_batch - self.pad_id)), reduction_indices=1)
   
   
   def build_encoder(self, encoder_input):
-    """TODO: add doc."""
-    
+    """Build the RNN stack for the encoder, depending on the initial config."""
     input_lengths = self.get_sequence_length(self.inputs)
+    
+    # We make only the first encoder layer bidirectional to capture the context
+    # (Wu et al., https://arxiv.org/pdf/1609.08144.pdf)
     if self.bidirectional_encoder:
       (encoder_fw_out, encoder_bw_out), _ = tf.nn.bidirectional_dynamic_rnn(
         self.rnn_cell(), self.rnn_cell(), encoder_input, dtype=tf.float32,
         sequence_length=input_lengths)
-      if self.add_fw_bw:
+      
+      # Postprocess the bidirectional output according to the initial config
+      if self.bidirectional_mode == 'add':
         encoder_output = encoder_fw_out + encoder_bw_out
       else:
-        encoder_output = tf.layers.dense(
-          tf.concat([encoder_fw_out, encoder_bw_out], 2), self.embedding_size,
-          activation=tf.tanh, name='bidirectional_projection')
+        encoder_output = tf.concat([encoder_fw_out, encoder_bw_out], 2)
+        if self.bidirectional_mode == 'project':
+          encoder_output = tf.layers.dense(
+            encoder_output, self.embedding_size, activation=tf.tanh,
+            name='bidirectional_projection')
     else:
       encoder_output, _ = tf.nn.dynamic_rnn(
         self.rnn_cell(), encoder_input, dtype=tf.float32,
         sequence_length=input_lengths)
+    
+    # Only for deep RNN architectures
     if self.rnn_layers > 1:
+      
       if self.pyramid_encoder:
         for i in xrange(1, self.rnn_layers):
           # Concatenate adjacent pairs and reshape them to their original size
@@ -131,6 +250,7 @@ class Seq2Seq(BaseModel):
           encoder_output = tf.layers.dense(
             tf.reshape(encoder_output, concat_shape), self.embedding_size,
             name='pyramid_projection_{}'.format(i))
+          # Run the next layer with half as many time steps
           encoder_output, _ = tf.nn.dynamic_rnn(
             self.rnn_cell(self.embedding_size / (2 ** i)),
             tf.reshape(encoder_output, concat_shape), dtype=tf.float32)
@@ -140,37 +260,55 @@ class Seq2Seq(BaseModel):
         encoder_output, _ = tf.nn.dynamic_rnn(
           encoder_cells, encoder_output, dtype=tf.float32,
           sequence_length=input_lengths)
+    
     return tf.contrib.seq2seq.tile_batch(encoder_output, self.beam_size)
   
   
   def build_decoder(self, encoder_output, decoder_input):
-    """TODO: add doc."""
+    """Build the decoder RNN stack and the final prediction layer."""
     
     # The first RNN is wrapped with the attention mechanism
-    if self.use_luong_attention:
-      attention_mechanism = tf.contrib.seq2seq.LuongAttention
-    else:
-      attention_mechanism = tf.contrib.seq2seq.BahdanauAttention
-    decoder_cell = self.rnn_cell(attention_mechanism=attention_mechanism(
-      self.embedding_size, encoder_output))
-    # Use last state of encoder to avoid wrong first outputs
+    # TODO: make Luong attention actually follow the computational steps
+    # described in the paper and implement input-feeding approach
+    # (Luong et al., https://arxiv.org/abs/1508.04025)
+    attention_mechanism = None
+    if self.attention == 'bahdanau':
+      attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(
+        self.embedding_size, encoder_output)
+    elif self.attention == 'luong':
+      attention_mechanism = tf.contrib.seq2seq.LuongAttention(
+        self.embedding_size, encoder_output)
+    
+    decoder_cell = self.rnn_cell(attention_mechanism=attention_mechanism)
+    
+    # Use the first output of the encoder to learn an initial decoder state
     initial_state_pass = tf.split(tf.layers.dense(
       encoder_output[:, 0], self.embedding_size * self.rnn_layers,
       activation=tf.tanh, name='initial_decoder_state'
     ), self.rnn_layers, axis=1)
-    beam_batch_size = self.batch_size * self.beam_size
-    initial_state = tf.contrib.seq2seq.AttentionWrapperState(
-      cell_state=initial_state_pass[0],
-      attention=tf.zeros([beam_batch_size, self.embedding_size]),
-      alignments=tf.zeros([beam_batch_size, self.max_decoder_length]),
-      time=tf.zeros(()), alignment_history=())
-    # Stack all the cells if more than one RNN is used
+    beam_batch_size = self.batch_size * self.beam_size  # shortcut
+    
+    if self.attention:
+      initial_state = tf.contrib.seq2seq.AttentionWrapperState(
+        cell_state=initial_state_pass[0],
+        attention=tf.zeros([beam_batch_size, self.embedding_size]),
+        alignments=tf.zeros([beam_batch_size, self.max_decoder_length]),
+        time=tf.zeros(()), alignment_history=())
+    else:
+      initial_state = initial_state_pass[0]
+    
+    # For deep RNNs, stack the cells and use an initial state that merges the
+    # initial state (maybe with attention) with the rest of the learned states
     if self.rnn_layers > 1:
       decoder_cell = tf.contrib.rnn.MultiRNNCell(
         [decoder_cell] + [self.rnn_cell()
                           for _ in xrange(self.rnn_layers - 1)])
       initial_state = tuple([initial_state] + list(initial_state_pass[1:]))
-    # Training decoder
+    
+    # Training decoder with optional scheduled sampling. If sampling occurs,
+    # the output will be fed through the final prediction layer and then fed
+    # back to the embedding layer for consistency.
+    # TODO: allow option to choose between embedding and output helpers
     sampling_helper = tf.contrib.seq2seq.ScheduledEmbeddingTrainingHelper(
       tf.contrib.seq2seq.tile_batch(decoder_input, self.beam_size),
       tf.tile([self.max_decoder_length], [beam_batch_size]),
@@ -181,7 +319,8 @@ class Seq2Seq(BaseModel):
       decoder_cell, sampling_helper, initial_state, output_layer=dense)
     decoder_output = tf.contrib.seq2seq.dynamic_decode(decoder)
     logits = decoder_output[0].rnn_output
-    # TODO: make this actually work
+    
+    # TODO: make beam search work
     # generative_decoder = tf.contrib.seq2seq.BeamSearchDecoder(
     #   decoder_cell, self.get_embeddings,
     #   tf.tile([self.go_id], [self.batch_size]), self.eos_id,
@@ -197,35 +336,3 @@ class Seq2Seq(BaseModel):
     generative_output = tf.contrib.seq2seq.dynamic_decode(
       generative_decoder, maximum_iterations=self.max_decoder_length)
     return logits, generative_output
-  
-  
-  def get_embeddings(self, ids):
-    """Performs embedding lookup. Useful as a method for decoder helpers."""
-    return tf.nn.embedding_lookup(self.embedding_kernel, ids)
-  
-  
-  def rnn_cell(self, num_units=None, attention_mechanism=None):
-    """Get a new RNN cell with wrappers according to the initial config."""
-    cell = None
-    if num_units is None:
-      num_units = self.embedding_size
-    if self.use_lstm:
-      cell = tf.contrib.rnn.LSTMBlockCell(num_units)
-    else:
-      cell = tf.contrib.rnn.GRUBlockCell(num_units)
-    if attention_mechanism is not None:
-      cell = tf.contrib.seq2seq.AttentionWrapper(cell, attention_mechanism)
-    if self.use_residual:
-      cell = tf.contrib.rnn.ResidualWrapper(cell)
-    if self.dropout < 1:
-      cell = tf.contrib.rnn.DropoutWrapper(
-        cell, input_keep_prob=self.dropout, output_keep_prob=self.dropout)
-    return cell
-  
-  
-  def get_sequence_length(self, sequence_batch):
-    """Given a 2D batch of input sequences, return a vector with the lengths
-       of every sequence excluding the paddings."""
-    with tf.name_scope('get_sequence_length'):
-      return tf.reduce_sum(
-        tf.sign(tf.abs(sequence_batch - self.pad_id)), reduction_indices=1)
