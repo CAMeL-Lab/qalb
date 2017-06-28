3021c5144aa0f51ec6498ff4af0ad821db3cad3f
Daniel Watson <watsondaniel6@gmail.com>
2017-06-29 01:03:49 +0400

    Added embedding trainer to module imports

--- ai/models/__init__.py
+++ ai/models/__init__.py
@@ -12,3 +12,4 @@ from ai.models.base_model import BaseModel
 #from ai.models.dnc import DNC
 from ai.models.next_gram import NextGram
 from ai.models.seq2seq import Seq2Seq
+from ai.models.softmax_embeddings import SoftmaxEmbeddings
