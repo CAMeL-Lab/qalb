cb07d9728d75b41a0bf04b5dd346c20f08db139e
Daniel Watson <watsondaniel6@gmail.com>
2017-10-28 22:59:36 +0400

    Added length checks

--- levenshtein.py
+++ levenshtein.py
@@ -10,6 +10,14 @@ with io.open(sys.argv[1], encoding='utf-8') as f:
 with io.open(sys.argv[2], encoding='utf-8') as f:
   gold_lines = f.readlines()
 
+try:
+  assert len(proposed_lines) == len(gold_lines)
+except AssertionError:
+  print("Mismatch in number of lines.")
+  print("Proposed length:", len(proposed_lines))
+  print("Gold length:", len(gold_lines))
+  exit()
+
 lev_densities = []
 for proposed, gold in zip(proposed_lines, gold_lines):
   lev_densities.append(editdistance.eval(proposed, gold) / len(gold))
